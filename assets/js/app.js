058dc91c0d14e4530b81c219fef64415899acca9
Masaki <masaki-miwa@users.noreply.github.com>
2017-05-10 09:09:59 +1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ $(function() {
     'MY SHOES ARE BLACK',
     'BED LIGHT DISTRICT',
     'HOVER',
-    'BOYS DON&rsquo;T POO, GIRLS DON&rsquo;T PEE',
+    'BOYS DON&rsquo;T POOH, GIRLS DON&rsquo;T P',
     '瞬間、心、重ねて',
     '深夜二時五十分以降に暴れ出す',
     '恋はいつでもハリケーン',
